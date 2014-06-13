942c6f6d0ae045ee7f0d248525636e73da563c44
tcnksm <nsd22843@gmail.com>
2014-06-13 17:02:31 +0900

    Update version

--- docc.rb
+++ docc.rb
@@ -2,14 +2,14 @@ require "formula"
 
 class Docc < Formula
   homepage "https://github.com/tcnksm/docc"
-  version '0.1.0'
+  version '0.1.1'
 
   if Hardware.is_64_bit?
-    url "https://github.com/tcnksm/docc/releases/download/v0.1.0/docc_0.1.0_darwin_amd64.zip"
-    sha1 "7b987855cccf3a030bc4f529b0e2cc8dd10cb916" # $ shasum docc_0.1.0_darwin_amd64.zip
+    url "https://github.com/tcnksm/docc/releases/download/v0.1.1/docc_0.1.1_darwin_amd64.zip"
+    sha1 "eaad2915415c5ceb3e3fb613420be62a856da46a" # $ shasum docc_0.1.0_darwin_amd64.zip
   else
-    url "https://github.com/tcnksm/docc/releases/download/v0.1.0/docc_0.1.0_darwin_386.zip"
-    sha1 "965b03cfa75bf2afc21788f3cfd40cfee1b4fe7a"
+    url "https://github.com/tcnksm/docc/releases/download/v0.1.1/docc_0.1.1_darwin_386.zip"
+    sha1 "6d8558d8589d7dcf034bf76035da1868248ddccc"
   end
 
   depends_on :arch => :intel
